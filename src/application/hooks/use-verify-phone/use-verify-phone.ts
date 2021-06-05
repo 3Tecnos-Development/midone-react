bbde3bc755b515e22b2e4df704a4108905f7fcdb
Lindsay Cerqueira <lindsay@3tecnos.com.br>
2021-06-05 12:03:41 -0300

    feat: add hook useVerifyPhone

--- src/application/hooks/use-verify-phone/use-verify-phone.ts
+++ src/application/hooks/use-verify-phone/use-verify-phone.ts
@@ -1,14 +1,31 @@
+import { ConfirmCodeService } from "core/services/use-cases/verified-phone/confirm-code";
 import { SendSMSService } from "core/services/use-cases/verified-phone/send-sms";
 
 interface IVerifyPhone {
-  sendSMS: () => void;
+  sendSMS: () => Promise<void>;
+  confirmCode: (verificationCode: string) => Promise<boolean>;
 }
+
 export const useVerifyPhone = (phoneNumber: string): IVerifyPhone => {
   const sendSMS = async (): Promise<void> => {
-    await SendSMSService.sendTo(phoneNumber);
+    await SendSMSService.handle(phoneNumber);
+  };
+
+  const confirmCode = async (
+    verificationCode: string,
+    twoFactor?: boolean,
+  ): Promise<boolean> => {
+    const { verified } = await ConfirmCodeService.handle({
+      phoneNumber,
+      verificationCode,
+      twoFactor,
+    });
+
+    return Promise.resolve(verified);
   };
 
   return {
     sendSMS,
+    confirmCode,
   };
 };
