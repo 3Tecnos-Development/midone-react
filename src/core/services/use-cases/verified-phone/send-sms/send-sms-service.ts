ba4ff788b2a4c2fd91a95d6cff1267d4ec26d4dc
Lindsay Cerqueira <lindsay@3tecnos.com.br>
2021-06-05 12:08:33 -0300

    feat: add SendSMSService

--- src/core/services/use-cases/verified-phone/send-sms/send-sms-service.ts
+++ src/core/services/use-cases/verified-phone/send-sms/send-sms-service.ts
@@ -2,7 +2,7 @@ import { ISendSMSUseCase } from "core/domain/use-cases/verified-phone";
 import { HttpAdapter } from "infra/http/http-adapter";
 
 export const SendSMSService: ISendSMSUseCase = class {
-  static async sendTo(phoneNumber: string): Promise<void> {
+  static async handle(phoneNumber: string): Promise<void> {
     await HttpAdapter.post("sms/send-to-confirmation", {
       phoneNumber,
     });
