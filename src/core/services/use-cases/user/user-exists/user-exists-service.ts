23c772b5f7f6765cf2a72a8346a06e145be03807
Lindsay Cerqueira <lindsay@3tecnos.com.br>
2021-06-05 12:05:58 -0300

    feat: add UserExistsService

--- src/core/services/use-cases/user/user-exists/user-exists-service.ts
+++ src/core/services/use-cases/user/user-exists/user-exists-service.ts
@@ -1,17 +1,16 @@
+import { UserExistsResponse } from "core/domain/models/user/user-exists/response/user-exists-response";
 import { IUserExistsUseCase } from "core/domain/use-cases/user";
 import { IHttpResponse } from "flawproof";
 import { HttpAdapter } from "infra/http/http-adapter";
 
-type Result = {
-  exists: boolean;
-};
-
 export const UserExistsService: IUserExistsUseCase = class {
-  static async exists(userName: string): Promise<boolean> {
+  static async handle(userName: string): Promise<UserExistsResponse> {
     const response = await HttpAdapter.get<IHttpResponse>(
       `users/${userName}/exists`,
     );
-    const { exists } = (response.data.body as unknown) as Result;
-    return Promise.resolve(exists);
+
+    return Promise.resolve(
+      (response.data.body as unknown) as UserExistsResponse,
+    );
   }
 };
